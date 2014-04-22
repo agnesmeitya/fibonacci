6b7a223e8aaac1161465badc740e21d2a75b1ee6
agnesmeitya <agnesmeityaaa@gamil.com>
2014-04-22 17:00:42 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class Fibonaci {
+public static void main (String [] args) {
+        Scanner scan=new Scanner (System.in);
+        System.out.print("input :");
+	 int x=0, y=1,deretfibonaci=scan.nextInt();
+	 for(int i=1; i<=deretfibonaci; i++) {
+		System.out.println("fibonacci :" +x );
+		x=x+y;
+		y=x-y;
+	
+		}
+	}
+}
